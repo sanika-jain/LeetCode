aa1aa1f41f7f3bc8a57c3121b08045e9ba61504a
sanika-jain <134360422+sanika-jain@users.noreply.github.com>
2025-03-05 20:38:29 +0530

    Update and rename 55. Jump Game to 55. Jump Game.java

--- 55. Jump Game.java
+++ 55. Jump Game.java
@@ -1,4 +1,4 @@
-Problem Statement: 
+/*Problem Statement: 
 You are given an integer array, nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 Return true if you can reach the last index, or false otherwise.
 Example 1:
@@ -8,9 +8,9 @@ Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
 Constraints:
 1 <= nums.length <= 10^4
-0 <= nums[i] <= 10^5
+0 <= nums[i] <= 10^5*/
 
-Solution:
+//Solution:
 class Solution {
     public boolean canJump(int[] nums) {
         int maxReach=0;
@@ -25,9 +25,9 @@ class Solution {
     
 }
 
-Explanation:
+/*Explanation:
 This greedy approach works with the complexity of O(N).
 Step 1 - Maintain a variable maxReach to calculate the maximum reach of the jump.
 Step 2 - Iter over the nums until you reach the end of the array.
 Step 3 - If i > maxReach, i.e, we cannot move forward.
-Step 4 - Update the farthest reacheable index , Math.max(maxReach,i+nums[i]).
+Step 4 - Update the farthest reacheable index , Math.max(maxReach,i+nums[i]).*/
