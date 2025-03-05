37d0d9cce19bb3cc3889c1f855afba17f32b98ea
sanika-jain <134360422+sanika-jain@users.noreply.github.com>
2025-03-05 20:40:20 +0530

    Java

--- 21. Merge Two Sorted Lists.java
+++ 21. Merge Two Sorted Lists.java
@@ -1,10 +1,10 @@
-Problem Statement: 
+/*Problem Statement: 
 You are given the heads of two sorted linked lists list1 and list2. Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
 Return the head of the merged linked list.
 Input: list1 = [1,2,4], list2 = [1,3,4]
-Output: [1,1,2,3,4,4]
+Output: [1,1,2,3,4,4]*/
 
-Solution:
+//Solution:
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode nn = new ListNode(); //node
@@ -30,7 +30,7 @@ class Solution {
     }
 }
 
-Expalanation:
+/*Expalanation:
 Initialize a dummy node (nn) to simplify handling the head of the merged list.
 Use head as a pointer starting from nn, which helps traverse and build the merged list.
 Iterate through both lists (list1 and list2) while neither is null:
@@ -38,4 +38,4 @@ Iterate through both lists (list1 and list2) while neither is null:
   Append the smaller node to head.next and move the corresponding list forward.
   Move head to the next position.
 Attach any remaining nodes from list1 or list2 (whichever is not null).
-Return nn.next, as nn itself is just a placeholder.
+Return nn.next, as nn itself is just a placeholder.*/
